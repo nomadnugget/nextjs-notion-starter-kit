087f054bc75444cf052e7c36582294954ba5ff48
nomadnugget <nomadnugget.kr@gmail.com>
2025-04-11 05:09:12 +0900

    Remove PageActions import and component, fix PageAside for centered content

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -2,8 +2,6 @@ import { type Block, type ExtendedRecordMap } from 'notion-types'
 
 import { getPageTweet } from '@/lib/get-page-tweet'
 
-import { PageActions } from './PageActions'
-
 export function PageAside({
   block,
   recordMap,
@@ -17,17 +15,22 @@ export function PageAside({
     return null
   }
 
-  // 페이지가 블로그 포스트일 경우, PageActions를 보여줍니다.
+  // 페이지가 블로그 포스트일 경우, 페이지에 대한 트윗을 가져와서 표시합니다.
   if (isBlogPost) {
     const tweet = getPageTweet(block, recordMap)
     if (!tweet) {
       return null
     }
 
-    return <PageActions tweet={tweet} />
+    // 트윗이 있을 경우 페이지에서 트윗과 관련된 작업을 진행할 수 있습니다.
+    return (
+      <div>
+        {/* 여기서 트윗을 렌더링하거나 다른 콘텐츠를 추가할 수 있습니다 */}
+      </div>
+    )
   }
 
-  // 소셜 버튼 제거 후 콘텐츠 가운데 정렬
+  // 콘텐츠가 여기에 중앙 정렬됩니다
   return (
     <div style={centeredStyle}>
       {/* 콘텐츠가 여기에 중앙 정렬됩니다 */}
